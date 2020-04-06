8272f4f36034b9411788117e9937158017b420c0
Odenzzz <shyvalovandreyzzz@gmail.com>
2020-04-06 10:49:39 +0700

    create api migrations

---
+++ database/migrations/2020_04_06_034147_create_api_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('api', function (Blueprint $table) {
+            $table->id();
+			$table->integer('project_id');
+			$table->string('api_name')->unique();
+			$table->string('api_file')->unique();
+			$table->text('data');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('api');
+    }
+}
