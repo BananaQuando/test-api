9af697f9d85179ef0273789801050476999b8359
Odenzzz <shyvalovandreyzzz@gmail.com>
2020-04-06 11:29:35 +0700

    add bootstrap

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::group(
+	[
+		'namespace' => 'Api',
+		'prefix'    => ''
+	],
+	function () {
+
+		Route::resource('/', 'ApiController')->only(['index'])->names('api');
+
+
+	}
+);
